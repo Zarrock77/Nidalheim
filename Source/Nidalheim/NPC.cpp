df087f9faefc945951a8913896cd4b54442115ce
MelvynGras <melvyn.gras@epitech.eu>
2025-03-20 18:13:26 +0100

    feat: bind npc event

---
+++ Source/Nidalheim/NPC.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NPC.h"
+
+DEFINE_LOG_CATEGORY_STATIC(UNPCSub, Log, All);
+
+void UNPC::SendMessageToNPC(int valueTest)
+{
+    UE_LOG(UNPCSub, Warning, TEXT("Hello, world"));
+}
---
+++ Source/Nidalheim/NPC.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "NPC.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class NIDALHEIM_API UNPC : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void SendMessageToNPC(int valueTest);
+};
